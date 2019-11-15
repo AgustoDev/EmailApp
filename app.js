322753b258614dde5a2d87b88728a1912d8e4af6
Cjay <blackrider401@gmail.com>
2019-11-15 17:47:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const helper = require("./helper");
 var btoa = require("btoa");
 var atob = require("atob");
 
-const endPoint = "http://10.0.0.223:8080";
+const endPoint = "https://agustoevents.herokuapp.com";
 const fullMail = require("./views/design");
 
 var conn = mysql.createPool({
-    host: "remotemysql.com",
-    user: "pIEQA6MsL8",
-    password: "M7CmwPbOeF",
-    database: "pIEQA6MsL8"
+	host: "remotemysql.com",
+	user: "pIEQA6MsL8",
+	password: "M7CmwPbOeF",
+	database: "pIEQA6MsL8"
 });
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    sendTheMail();
-    res.send("Welcome to Email App");
+	sendTheMail();
+	res.send("Welcome to Email App");
 });
 
 app.use(express.static("public"));
@@ -29,30 +29,30 @@ app.use(express.static("public"));
 // });
 
 const sendTheMail = () => {
-    // get list from db
-    conn.query("select * from user WHERE status = 0", [], (err, result) => {
-        if (err) return;
+	// get list from db
+	conn.query("select * from user WHERE status = 0", [], (err, result) => {
+		if (err) return;
 
-        console.log(result.length);
+		console.log(result.length);
 
-        result.forEach(el => {
-            let link = `${endPoint}/event/${btoa(el.email)}`;
-            let msg = fullMail(link);
-            helper.sendEmail(el.email, "Invited We Are !", msg);
-        });
-    });
+		result.forEach(el => {
+			let link = `${endPoint}/event/${btoa(el.email)}`;
+			let msg = fullMail(link);
+			helper.sendEmail(el.email, "Invited We Are !", msg);
+		});
+	});
 };
 
 app.get("/event/:email/:action", function(req, res) {
-    let email = atob(req.params.email);
-    conn.query("update user set action=?,status=1 WHERE email=?", [req.params.action, email], (err, result) => {
-        if (err) {
-            return;
-        }
-        res.render("index", {
-            action: req.params.action
-        });
-    });
+	let email = atob(req.params.email);
+	conn.query("update user set action=?,status=1 WHERE email=?", [req.params.action, email], (err, result) => {
+		if (err) {
+			return;
+		}
+		res.render("index", {
+			action: req.params.action
+		});
+	});
 });
 
 const port = process.env.PORT || 11000;
